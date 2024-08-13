733db5676b29dca9e87638de90c459a7815aff9a
Leticia Lima <leticia_lele.lima@hotmail.com>
2024-08-13 14:37:25 -0300

    first commit

---
+++ src/main/java/edu/e_commerce/ECommerceApplication.java
@@ -0,0 +1,13 @@
+package edu.e_commerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/e_commerce/ECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.e_commerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
