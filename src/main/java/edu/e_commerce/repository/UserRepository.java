0ff9b045a80a6608ede6068457aad998ec2286e1
Leticia Lima <leticia_lele.lima@hotmail.com>
2024-08-14 19:59:01 -0300

    adionando a entidade usuario , a classe de servico , o repositorio e o controlador

---
+++ src/main/java/edu/e_commerce/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package edu.e_commerce.repository;
+
+import edu.e_commerce.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
